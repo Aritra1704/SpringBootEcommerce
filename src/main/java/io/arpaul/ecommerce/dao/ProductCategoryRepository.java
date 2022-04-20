a8d99ecf396d14a64dafd72db62a27b712ae7c38
ArPal <aritrarpal@gmail.com>
2022-04-20 19:00:54 +0530

    pagination added

--- src/main/java/io/arpaul/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/io/arpaul/ecommerce/dao/ProductCategoryRepository.java
@@ -3,10 +3,12 @@ package io.arpaul.ecommerce.dao;
 import io.arpaul.ecommerce.payload.entity.ProductCategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(
         collectionResourceRel = "productCategory",
         path = "product-category"
 )
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategoryEntity, Long> {
 }
